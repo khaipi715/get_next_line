7af29bd5448cb1cf8b3a35496d83e8061a9599f2
Lian Za Khai Naulak <lnaulak@F3C008.42adel.org.au>
2023-07-27 11:41:28 +0930

    cool

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lnaulak <lnaulak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 09:18:27 by lnaulak           #+#    #+#             */
-/*   Updated: 2023/07/24 13:18:36 by lnaulak          ###   ########.fr       */
+/*   Updated: 2023/07/27 11:38:22 by lnaulak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,24 +27,25 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-char	*read_function(int fd, char *str) // this only makes sense on if the str is\n empty if it is not empty then it we will find the douplication of it self 
+char	*read_function(int fd, char *str)
 {
 	char	*tmp;
-	int		bytes;
+	int		byte;
 
-	tmp = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	byte = 1;
+	tmp = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!tmp)
 		return (NULL);
-	bytes = 1;
-	while (ft_strchr(str, '\n') == 0 && (bytes != 0))
+	while (ft_strchr(str, '\n') == 0 && (byte != 0))
 	{
-		bytes = read(fd, tmp, BUFFER_SIZE);
-		if (bytes == -1)
+		byte = read(fd, tmp, BUFFER_SIZE);
+		if (byte == -1)
 		{
 			free(tmp);
+			free(str);
 			return (NULL);
 		}
-		tmp[bytes] = '\0';
+		tmp[byte] = '\0';
 		str = ft_strjoin(str, tmp);
 	}
 	free(tmp);
@@ -94,17 +95,15 @@ char	*ft_getrest(char *full_str)
 		return (NULL);
 	}
 	restof = (char *)malloc(sizeof(char) * (ft_strlen(full_str) - i + 1));
-	if (!restof)
-		return (NULL);
 	i++;
 	j = 0;
-	while (full_str[i]) // gives everything after the newline since i = newline position
+	while (full_str[i])
 		restof[j++] = full_str[i++];
 	restof[j] = '\0';
 	free(full_str);
 	return (restof);
 }
-
+/*
 int main(void)
 {
 	for (int i = 0; i < 2; i++)
@@ -113,4 +112,4 @@ int main(void)
 		printf("%s", get_next_line(fd));
 	}
 	return 0;
-}
+}*/
